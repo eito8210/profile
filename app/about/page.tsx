3dad9c0d05de106e8f1395d442517928fbd22e84
Eito Nishikawa <ena50@sfu.ca>
2024-10-27 21:23:31 -0700

    Added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+import Image from "next/image";
+
+
+export default function About() {
+  return (
+    <div className="mt-16">
+      This is Eito
+    </div>
+  );
+}
